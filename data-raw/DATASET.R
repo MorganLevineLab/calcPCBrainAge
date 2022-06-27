8a4a0bf93fe715e48d495af46edf17f10d988afa
kthrush <44170556+kthrush@users.noreply.github.com>
2022-06-27 19:47:32 -0400

    Delete data-raw directory

--- data-raw/DATASET.R
+++
@@ -1,8 +0,0 @@
-## code to prepare datas goes here
-load("data-raw/brainMeans.RData")
-imputeMissingBrainCpGs <- brainMeans
-usethis::use_data(imputeMissingBrainCpGs, overwrite = TRUE)
-
-load("data-raw/PCBrainAge.RData")
-PCBrainAge_Model <- PCBrainAge
-usethis::use_data(PCBrainAge_Model, overwrite = TRUE)
